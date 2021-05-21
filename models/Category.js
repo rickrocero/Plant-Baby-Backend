a635a9b1c8800df0aeb98431dc91b46a7a9726a3
Amanda N <amandavy.nguyen@bellevuecollege.edugit config --global user.email amandavy.nguyen@bellevuecollege.edu>
2021-05-20 20:40:53 -0700

    starting files

---
+++ models/Category.js
@@ -0,0 +1,2 @@
+// Categories would include "Pet friendly" "Low Maintenance" "Exotic" "Large" "Medium""Small"
+// Categories schema
---
+++ models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Category = require('./Category');
+const Plant = require('./Plant');
+
+
+module.exports = {User, Category, Plant};
