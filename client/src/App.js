beb8a54e81fbef82c535e1a3c2274c831f42f732
KKH-pixel <kendra.hunt@seattlecentral.edu>
2021-05-25 11:36:54 -0700

    updated file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar></NavBar>
     </div>
   );
-}
+};
 
 export default App;
