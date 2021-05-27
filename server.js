8710958960755a76adaaabb476047c4f8f856ca4
Cory Moe <moecory11@gmail.com>
2021-05-27 12:55:47 -0700

    wip

--- server.js
+++ server.js
@@ -1,39 +1,57 @@
-const express = require('express');
-const session = require('express-session');
-// const routes = require('./controllers');
-const path = require('path');
+// const express = require('express');
+// const session = require('express-session');
+// const allRoutes = require('./routes');
+// const path = require('path');
+// const PORT = process.env.PORT || 3001;
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const sequelize = require('./config/connection');
-const { User } = require('./models');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const sequelize = require('./config/connection');
+// const { User } = require('./models');
+// // const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const app = express();
+// const app = express();
 
-// const sess = {
-//     secret: process.env.SECRET,
-//     cookie: {
-//         maxAge: 1000 * 60 * 60 * 2
-//     },
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+// // app.use(session(sess));
 
-// app.use(session(sess));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
+// // app.use('/', allRoutes);
+
+// sequelize.sync({ force: false }).then(() => {
+//     app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+// });
+var express = require('express');
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3001;
+var allRoutes = require('./routes');
+// const cors = require("cors")
+
+// Requiring our models for syncing
+var sequelize = require('./config/connection.js');
+const {User} = require("./models");
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+//LOCAL
+// app.use(cors());
+
+//DEPLOYED
+// app.use(cors({
+//     origin:["https://marchfishfront.herokuapp.com"]
+// }));
+
+// app.use('/',allRoutes);
 
 
 
-
-// app.use(routes);
-
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
+sequelize.sync({ force: false }).then(function () {
+    app.listen(PORT, function () {
+        console.log('App listening on PORT ' + PORT);
+    });
 });
