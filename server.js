a3e39d1bf3843667bdc64cd45168d952b250dbc7
Cory Moe <moecory11@gmail.com>
2021-05-27 10:12:56 -0700

    update

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const path = require('path');
 
 require("dotenv").config();
 
 const sequelize = require('./config/connection');
-const { truncate } = require('./models/User');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { User } = require('./models');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 
-const sess = {
-    secret: process.env.SECRET,
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 2
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
+// const sess = {
+//     secret: process.env.SECRET,
+//     cookie: {
+//         maxAge: 1000 * 60 * 60 * 2
+//     },
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//         db: sequelize
+//     })
+// };
 
-app.use(session(sess));
+// app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', exphbs({ defaultLayout: "main" }));
-app.set('view engine', 'handlebars');
 
 
-app.use(routes);
+
+
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
